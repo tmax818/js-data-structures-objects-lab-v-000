a13a3eadb707ab2e849ff13f1a0d4eb1d9729d1d
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-05-21 02:03:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+let driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  let newDriver = { ...driver };
+  
+  newDriver[key] = value;
+  
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  
+  driver[key] = value;
+  
+  return driver;
+}
+
+function deleteFromDriverByKey({}, driver, key){
+  let newDriver = { ...driver };
+  
+  delete newDriver.key; 
+  
+  return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  
+  driver[key] = undefined;
+  
+  return driver;
+ 
+ 
+ 
+}
